192a5b68482bed72180ca041d7047edc1241adb0
LaRaye <lnj2105@gmail.com>
2020-05-02 23:05:36 -0400

    animal shelter node and linked list

--- animal_shelter.rb
+++ animal_shelter.rb
@@ -1,3 +1,58 @@
+class Node
+  attr_accessor :next, :value
+
+  def initialize(value = nil)
+    @value = value
+    @next  = nil
+  end
+end
+
+class LinkedList
+  attr_reader :head
+
+  def initialize
+    @head = nil
+  end
+
+  def head=(node)
+    if @head
+      node.next = @head
+      @head = node
+    else
+      @head = node
+    end
+  end
+
+  def delete(value)
+    if @head.value == value
+      @head = @head.next
+      return
+    end
+
+    node = find_before(value)
+    node.next = node.next.next
+  end
+
+  def find_before(value)
+    node = @head
+    return false if !node.next
+    return node  if node.next.value == value
+
+    while (node = node.next)
+      return node if node.next && node.next.value == value
+    end
+  end
+
+  def print
+    node = @head
+    while node
+      puts node.value
+      node = node.next
+    end
+  end
+end
+
+
 class Animal
   attr_accessor :timestamp
 
