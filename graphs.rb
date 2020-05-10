5517faeaf691c3cbc828f45d49cd0a043fcea028
LaRaye <lnj2105@gmail.com>
2020-05-09 22:08:33 -0400

    graphs node class

--- graphs.rb
+++ graphs.rb
@@ -1,3 +1,15 @@
+class Node
+  attr_accessor :value, :left, :right
+
+  def initialize(value = nil)
+    @value = value
+    @left  = nil
+    @right = nil
+  end
+end
+
+
+
 #Given a directed graph, design an algorithm to find out
 #whether there is a route between two nodes.
 
@@ -10,5 +22,7 @@
 #    D
 
 def is_a_route?(graph, start_node, end_node)
+  return true if start_node == end_node
+
 
 end
